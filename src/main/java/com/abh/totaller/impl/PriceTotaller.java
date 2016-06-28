0b1e2ca61681815d7baf015a3075eb4b4bfbb598
alexyzhart <alexyzhart@yahoo.com>
2016-06-28 16:32:07 +0100

    Now uses accumulateAndGet
    Now uses accumulateAndGet

--- src/main/java/com/abh/totaller/impl/PriceTotaller.java
+++ src/main/java/com/abh/totaller/impl/PriceTotaller.java
@@ -11,7 +11,9 @@ public class PriceTotaller implements IPriceTotaller {
     @Override
     public BigDecimal calcTotalPrice(Collection<IItem> basketOfItems){
         final AtomicReference<BigDecimal> total = new AtomicReference(BigDecimal.ZERO);
-        basketOfItems.forEach(item -> total.set(total.get().add(item.getPrice())));
+
+        basketOfItems.forEach(item->{total.accumulateAndGet(item.getPrice(), (bd1, bd2) -> bd1.add(bd2));});
+
         return total.get();
     }
 }
